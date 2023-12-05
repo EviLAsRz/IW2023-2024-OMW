37dac48d44d5b318493348013b2077f6ccc0f0de
Alejandro Guerrero Medina <alejandro.guerreromedina@alum.uca.es>
2023-12-05 17:20:11 +0100

    Tweaks Modelo Cliente

--- src/main/java/com/IWPhone/Models/Cliente.java
+++ src/main/java/com/IWPhone/Models/Cliente.java
@@ -10,81 +10,66 @@ import java.util.UUID;
 @Entity
 public class Cliente{
 
+    //_Id: Métodos.
     @GeneratedValue
     @Id
-    private UUID id;
-
-    @NotEmpty
-    private String _nombre;
-
-    @NotEmpty
-    private String _apellidos;
-
-    @NotEmpty
-    private String _dni;
-
-    @NotEmpty
-    private String _direccion;
-
-    @NotEmpty
-    private String _telefonoMovil;
-
-    private String _telefonoFijo = null;
-
+    private UUID _Id;
     public UUID getId() {
-        return id;
+        return _Id;
+    }
+    /* TODO: ¿Realmente nos hace falta un setId? Quiero decir, es un método que al estar modificándose
+        algo tan crucial como es el identificador puede dar problemas, ¿no?
+        public void setId(UUID id) { this.Id = id; } */
+
+    //_sName: Métodos.
+    @NotEmpty
+    private String _sName;
+    public String getName() { return _sName; }
+    public void setName(String name) {
+        if(name == null || name.isEmpty()) throw new IllegalArgumentException(
+                "Nombre no puede ser nulo ni vacío");
+        _sName = name;
     }
 
-    public void setId(UUID id) {
-        this.id = id;
+    //_sLastName: Métodos.
+    @NotEmpty
+    private String _sLastName;
+    public String getLastName() { return _sLastName; }
+    public void setLastName(String lastname) {
+        if(lastname == null || lastname.isEmpty()) throw new IllegalArgumentException(
+                "Apellido no puede ser nulo ni vacío");
+        _sLastName = lastname;
     }
 
-    public String get_nombre() {
-        return _nombre;
+    @NotEmpty
+    private String _sDNI;
+    public String getDNI() { return _sDNI; }
+    public void setDNI(String dni) {
+        if(dni == null || dni.isEmpty()) throw new IllegalArgumentException(
+                "DNI no puede ser nulo ni vacío");
+        _sDNI = dni;
     }
 
-    public void set_nombre(String _nombre) {
-        this._nombre = _nombre;
+    @NotEmpty
+    private String _sAddress;
+    public String getAddress() { return _sAddress; }
+    public void setAddress(String address) {
+        if(address == null || address.isEmpty()) throw new IllegalArgumentException(
+                "Dirección no puede ser nula ni vacía");
+        _sAddress = address;
     }
 
-    public String get_apellidos() {
-        return _apellidos;
+    @NotEmpty
+    private String _sMobilePhone;
+    public String getMobilePhone() { return _sMobilePhone; }
+    public void setMobilePhone(String mobilePhone) {
+        if(mobilePhone == null || mobilePhone.isEmpty()) throw new IllegalArgumentException(
+                "Teléfono móvil no puede ser nulo ni vacío");
+        _sMobilePhone = mobilePhone;
     }
 
-    public void set_apellidos(String _apellidos) {
-        this._apellidos = _apellidos;
-    }
-
-    public String get_dni() {
-        return _dni;
-    }
-
-    public void set_dni(String _dni) {
-        this._dni = _dni;
-    }
-
-    public String get_direccion() {
-        return _direccion;
-    }
-
-    public void set_direccion(String _direccion) {
-        this._direccion = _direccion;
-    }
-
-    public String get_telefonoMovil() {
-        return _telefonoMovil;
-    }
-
-    public void set_telefonoMovil(String _telefonoMovil) {
-        this._telefonoMovil = _telefonoMovil;
-    }
-
-    public String get_telefonoFijo() {
-        return _telefonoFijo;
-    }
-
-    public void set_telefonoFijo(String _telefonoFijo) {
-        this._telefonoFijo = _telefonoFijo;
-    }
+    private String _sLandline = null;
+    public String getLandline() { return _sLandline; }
+    public void setLandline(String landLine) { _sLandline = landLine; }
 }
 
