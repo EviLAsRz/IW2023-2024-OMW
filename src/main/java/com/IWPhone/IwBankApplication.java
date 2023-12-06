27ef66faff534bb65df502c72d1275ea426dd53e
Antonio Roldán Andrade <48240649+Mry3ll0w@users.noreply.github.com>
2023-12-06 23:41:02 +0100

    Fix properties

--- src/main/java/com/IWPhone/IwBankApplication.java
+++ src/main/java/com/IWPhone/IwBankApplication.java
@@ -22,11 +22,11 @@ public class IwBankApplication {
 
         return (args) -> {
             // save a couple of customers
-            /*repository.save(new Customer("Jack", "Bauer"));
+            repository.save(new Customer("Jack", "Bauer"));
             repository.save(new Customer("Chloe", "O'Brian"));
             repository.save(new Customer("Kim", "Bauer"));
             repository.save(new Customer("David", "Palmer"));
-            repository.save(new Customer("Michelle", "Dessler"));*/
+            repository.save(new Customer("Michelle", "Dessler"));
 
             // fetch all customers
             /*log.info("Customers found with findAll():");
