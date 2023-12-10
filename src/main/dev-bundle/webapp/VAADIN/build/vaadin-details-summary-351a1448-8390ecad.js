bfdfe0b51760154e54929434358b7043b4e38a84
EviLAsRz <alvarosantosrom9@gmail.com>
2023-12-10 06:40:39 +0100

    login casi listo

---
+++ src/main/dev-bundle/webapp/VAADIN/build/vaadin-details-summary-351a1448-8390ecad.js
@@ -0,0 +1,1 @@
+import{O as a,I as t,b as e}from"./indexhtml-c476f4d9.js";const s={tagName:"vaadin-details-summary",displayName:"Details Summary",elements:[{selector:"vaadin-details-summary",displayName:"Summary",properties:[a.textColor,a.fontSize,t.padding]},{selector:"vaadin-details-summary::part(toggle)",displayName:"Toggle",properties:[e.iconColor,e.iconSize]}]};export{s as default};
--- src/main/java/com/IWPhone/login/views/LoginView.java
+++ src/main/java/com/IWPhone/login/views/LoginView.java
@@ -1,40 +1,85 @@
 package com.IWPhone.login.views;
 
+import com.vaadin.flow.component.dependency.CssImport;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.component.textfield.PasswordField;
+import com.vaadin.flow.component.formlayout.FormLayout;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.html.H1;
-import com.vaadin.flow.component.login.LoginForm;
+import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.BeforeEnterEvent;
 import com.vaadin.flow.router.BeforeEnterObserver;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.server.auth.AnonymousAllowed;
+import com.vaadin.flow.component.button.Button;
+import com.IWPhone.registration.views.ClientRegistration;
+
+import java.awt.*;
 
 @Route("login")
 @PageTitle("Acceso de Usuario IW")
 @AnonymousAllowed
+@CssImport("./loginStyle.css")
 public class LoginView extends VerticalLayout implements BeforeEnterObserver {
 
-    private final LoginForm login = new LoginForm();
+    FormLayout formLayout = new FormLayout();
+    TextField usernameField = new TextField();
+    PasswordField passwordField = new PasswordField();
+    Button loginButton = new Button("continuar");
 
     public LoginView(){
+
+        //username settings
+        usernameField.setRequiredIndicatorVisible(true);
+        usernameField.setMinWidth("400px");
+        usernameField.setMinLength(9);
+        usernameField.setMaxLength(9);
+        usernameField.setLabel("Dni");
+        formLayout.add(usernameField);
+
+        //password settings
+        passwordField.setRequiredIndicatorVisible(true);
+        passwordField.setMinWidth("400px");
+        passwordField.setMinLength(8);
+        passwordField.setMaxLength(50);
+        passwordField.setLabel("Contraseña");
+        formLayout.add(passwordField);
+
         addClassName("login-view");
         setSizeFull();
         setAlignItems(Alignment.CENTER);
         setJustifyContentMode(JustifyContentMode.CENTER);
 
-        login.setAction("login");
+        //form layout styles
+        formLayout.addClassName("form-layout");
 
-        add(new H1("Acceso de Usuario"), login);
+        VerticalLayout loginLayout = new VerticalLayout(
+                new H2("Iniciar sesión"),
+                usernameField,
+                passwordField
+        );
+        loginLayout.setAlignItems(Alignment.CENTER);
+        loginLayout.setJustifyContentMode(JustifyContentMode.CENTER);
+        
+        loginButton.addClickListener(e -> {
+            String dniValue = usernameField.getValue();
+            //Test if user exists!!
+            //createUser(usernameField.getValue(), passwordField.getValue());
+
+        });
+
+        add(formLayout, loginLayout, loginButton);
     }
 
     @Override
-    public void beforeEnter(BeforeEnterEvent beforeEnterEvent) {//NegusCode, precondiciones magicas.
+    public void beforeEnter(BeforeEnterEvent beforeEnterEvent) {
         // inform the user about an authentication error
         if(beforeEnterEvent.getLocation()
                 .getQueryParameters()
                 .getParameters()
                 .containsKey("error")) {
-            login.setError(true);
         }
     }
 }
